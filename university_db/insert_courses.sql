0e3f27eddd1cba452be64d0cb7ba29436758d9ac
Shifty Shei I. Banogbanog <banogbanogshifty_bsit@plmun.edu.ph>
2025-03-14 18:49:54 +0800

    Fixed data issues for post-execution check

---
+++ university_db/insert_courses.sql
@@ -0,0 +1,11 @@
+-- Select the correct database
+USE `UniversityDB`;
+
+-- Step 1: Insert data into the Courses table
+INSERT INTO `Courses` (`CourseName`)
+VALUES
+    ('Introduction to Computer Science'),
+    ('Calculus I'),
+    ('Modern World History'),
+    ('English Literature'),
+    ('Data Structures');
